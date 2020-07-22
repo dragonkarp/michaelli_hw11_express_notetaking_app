5f6933079f6c452815304c121438922add6e44f5
Michael Li <dev.mike011235@gmail.com>
2020-07-22 11:25:59 -0400

    Added app.get functions

---
+++ Develop/public/assets/js/backend.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+
+const app = express()
+const PORT = 3643
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Load main page.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./index.html"))
+})
+
+// Load notes page.
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./notes.html"))
+})
+
+// Read db.json and returns all saved notes as a JSON.
+app.get("api/notes", (req, res) => {
+    return
+})
+
+
+// Server is active. Listening on PORT.
+app.listen(PORT, () => {
+    console.log("App listening on port: " + PORT)
+})
